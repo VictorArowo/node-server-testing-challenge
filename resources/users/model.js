68d4f62c04d6e51e26a10c8f943aa968ef4a8055
Victor Arowo <arowovici@gmail.com>
2019-11-14 15:24:20 +0100

    Added Middlewares, Controllers and Models for Users Resource

---
+++ resources/users/model.js
@@ -0,0 +1,27 @@
+const db = require('../../data/dbConfig');
+
+exports.getAllUsers = department => {
+  return db('users').select('users.id', 'users.username', 'users.department');
+};
+
+exports.getUser = username => {
+  return db('users')
+    .where('username', '=', username)
+    .select('users.id', 'users.username', 'users.department')
+    .first();
+};
+
+exports.postUser = async user => {
+  return db('users')
+    .insert(user, 'id')
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
+};
+
+function findById(id) {
+  return db('users')
+    .where({ id })
+    .first();
+}
